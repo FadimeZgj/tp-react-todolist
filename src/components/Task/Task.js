80244b9b82d4f8cff7750e15927fefebffdea038
FadimeZgj <fadime.ilhan5703@gmail.com>
2023-12-17 18:44:35 +0100

    Ajout fonctionnalité modification de tâche

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,42 +1,51 @@
 import React, { useEffect, useState } from "react";
 
-function Task({ taskProps, id, onDelete }) {
-  const [status, setStatus] = useState(taskProps.isCompleted); // état du statut de la tâche 
-  const [color, setColor] = useState("#fff"); // état de la couleur de fond initiale 
+function Task({ taskProps, id, onDelete, onEdit }) {
+  const [status, setStatus] = useState(taskProps.isCompleted);
+  const [color, setColor] = useState("#fff");
+  const [userInput, setUserInput] = useState(taskProps.taskname);
 
-  // changer le statut de la tâche
+  // Changement du statut d'une tâche
   const handleChangeStatus = () => {
-    if (status === false) {
-      setStatus(true);
-    } else if (status === true) {
-      setStatus(false);
-    }
+    setStatus(!status);
   };
 
-  // changement de la couleur de fond en fonction du statut
+  // Changement de la couleur du background selon le statut de la tâche
   useEffect(() => {
-    if (status === true) {
-      setColor("#99c199");
-    } else {
-      setColor("#fff");
-    }
+    setColor(status ? "#99c199" : "#fff");
   }, [status]);
 
-  // supression d'une tâche
+  // suppression d'une tâche
   const handleDelete = () => {
     onDelete(id);
+  };
+
+  // Modification d'une tâche
+  const handleEdit = () => {
+    onEdit(id, userInput); // Modification ici pour transmettre la nouvelle valeur de l'input
   };
 
   return (
     <>
       <div className="p-2" style={{ backgroundColor: color }}>
         <p className="fs-4">{taskProps.taskname}</p>
-        <small className="fw-bold">{status === false ? "En cours" : "Fait"}</small>
+        <small className="fw-bold">{status ? "Fait" : "En cours"}</small>
       </div>
-      <button className={status?"col-3 me-5 btn btn-warning":"col-3 me-5 btn btn-success"} onClick={(e) => handleChangeStatus(e.target.value)}>
+
+      <div className="row">
+      <button
+        className={status ? "col-3 me-5 btn btn-warning" : "col-3 me-5 btn btn-success"}
+        onClick={handleChangeStatus}
+      >
         {status ? "Reprendre la tâche" : "Terminer la tâche"}
       </button>
-      <button className="col-3 btn btn-outline-danger" onClick={() => handleDelete(id)}>Supprimer</button>
+      <div className="col-3">
+      <input className="form-control" type="text" onChange={(e) => setUserInput(e.target.value)} value={userInput}></input></div>
+      <button className="col-2 btn btn-outline-primary me-5" onClick={handleEdit}>Modifier</button>
+      <button className="col-2 btn btn-outline-danger" onClick={() => handleDelete(id)}>
+        Supprimer
+      </button>
+      </div>
     </>
   );
 }
--- src/components/Task/TaskList.js
+++ src/components/Task/TaskList.js
@@ -3,7 +3,7 @@ import TaskProps from "./TaskProps";
 import NewTaskForm from "./NewTaskForm";
 import Task from "./Task";
 
-// liste des tâches
+// Liste des tâches initiale
 let tasksList = [
   new TaskProps(0, "Tâche 1"),
   new TaskProps(1, "Tâche 2"),
@@ -14,31 +14,41 @@ function TaskList() {
 
   // Ajout d'une nouvelle tâche
   const handleNewTask = (data) => {
-    let newTaskList = [...list]; // clonage de la liste
-    newTaskList.push(data); // ajout de la nouvelle tâche dans la liste clonée
-    setList(newTaskList); // changement de l'état de la liste avec le tableau cloné
+    let newTaskList = [...list];
+    newTaskList.push(data);
+    setList(newTaskList);
   };
 
-  // supression d'une tâche
+  // Suppression d'une tâche
   const handleTaskDelete = (id) => {
     let newTaskList = [...list];
     newTaskList = newTaskList.filter((elem) => elem.id !== id);
     setList(newTaskList);
   };
-  
+
+  // Edition d'une tâche
+  const handleTaskEdit = (id, updatedTaskName) => {
+    // Recherche de l'élément à éditer
+    const updatedList = list.map((item) =>
+      item.id === id ? { ...item, taskname: updatedTaskName } : item
+    );
+    setList(updatedList); //maj de l'état de list
+  };
+
   return (
     <>
       <div className="row">
         <div className="col-6 mb-4">
-        <NewTaskForm onSubmit={(data) => handleNewTask(data)} />
+          <NewTaskForm onSubmit={(data) => handleNewTask(data)} />
         </div>
         <ul className="list-group text-center">
           {list.map((task) => (
-            <li className="list-group-item" key={task.id} style={{backgroundColor: task.isCompleted ? "green" : ""}}>
+            <li className="list-group-item" key={task.id} style={{ backgroundColor: task.isCompleted ? "green" : "" }}>
               <Task
                 id={task.id}
                 taskProps={task}
                 onDelete={() => handleTaskDelete(task.id)}
+                onEdit={(id, updatedTaskName) => handleTaskEdit(id, updatedTaskName)}
               />
             </li>
           ))}
